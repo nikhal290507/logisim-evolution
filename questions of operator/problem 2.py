785417d778bbdbe2b5377a59d898b7aa71234008
nikhal <nikhalkumar0110@gmail.com>
2025-08-27 09:28:34 +0530

    Add files via upload

---
+++ questions of operator/problem 2.py
@@ -0,0 +1,17 @@
+a=5
+b=10
+print("before swaping a=",a,"b =",b)
+
+a=a+b
+b=a-b
+a=a-b
+print("after swaping a =",a,"b =",b)
+
+a=int(input("entre the value :"))
+b=int(input("entre the value :"))
+print("before swapping a=",a,"b=",b)
+
+a=a+b
+b=a-b
+a=a-b
+print("aftre swapping a=",a,"b=",b)
