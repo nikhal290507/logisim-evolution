0e3f5435b015d3971cc3f31c1699147fcd4ea6ab
nikhal <nikhalkumar0110@gmail.com>
2025-09-01 21:30:56 +0530

    Add files via upload

---
+++ day7functions/functions.py
@@ -0,0 +1,41 @@
+# a function is a block of code that preform a specific task
+# # it helps in code reusability
+# we can use a specific code to any thousand palces without retyping it
+
+
+#syntax
+
+#def function_name(parameters):
+    #function body
+    #return value
+
+
+
+# def greet(name):
+#     return f"hello , {name}"
+# print(greet("nikhil"))
+
+# num=[10,20,30]
+# print(len(num))
+# print(max(num))
+# print(min(num))
+# print(sum(num))
+
+
+# def square(x):
+#     return x*x
+# print(square(5))//calling function
+
+
+# def add(a,b):
+#     return (a+b)
+# res=add(10,20)
+# print("the addition of a and b =",res)
+
+a=int(input("entre the value of a :"))
+b=int(input("entre the value of b :"))
+def add(a,b):
+    return (a+b)
+res=add(a,b)
+print("the addition of a and b =",res)
+
